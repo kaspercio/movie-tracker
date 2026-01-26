24077a2a111a7908007d39355a05a8111b90d3df
kaspercio <147722983+kaspercio@users.noreply.github.com>
2026-01-26 20:51:16 +0000

    Migrated data from MySQL to Postgres and added Docker & Docker compose config

--- app/database.py
+++ app/database.py
@@ -11,8 +11,7 @@ class Base(DeclarativeBase):
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}  # Only needed for SQLite
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,8 @@
 from app.database import engine, Base
+from app.models import User, Movie
+from dotenv import load_dotenv
+
+load_dotenv()
 
 Base.metadata.create_all(bind=engine)
-
 print("Database tables created successfully!")
